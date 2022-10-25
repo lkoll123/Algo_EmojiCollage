cc358094b95e8c702269efc806b300c5cb813228
lkoll123 <110080569+lkoll123@users.noreply.github.com>
2022-10-25 12:09:48 -0400

    Create EmojiRGB.py

---
+++ EmojiRGB.py
@@ -0,0 +1,7 @@
+#Function that uses cropSquare ad emojiread functions to crop, and then append averageRGB values for all emoji
+
+#this list will be a list of lists, holding the averageRGB values of all emojis in a folder
+averageRGB = []
+
+def emojiRGB(inputarr):
+  
