ddbfd6e9bbc3329dae9b5aca2bd9235c1966b0c9
vangory <112182667+vangory@users.noreply.github.com>
2022-10-26 14:02:58 -0400

    Update Squarecrop.py

--- Squarecrop.py
+++ Squarecrop.py
@@ -1,1 +1,14 @@
+def cropSquare(im,size):
+    resizeImage = Image.new("RGB", (size,size))
 
+    if (im.width < im.height):
+        scale = size/im.width
+    else:
+        scale = size/im.height
+    
+    for i in range(size):
+        for j in range(size):
+            r,g,b = im.getpixel((i/scale, j/scale))
+            resizeImage.putpixel((i,j), (r,g,b))
+
+    return resizeImage
