ee9e4f3b7b83e76d2a970feeeb23927299283162
vangory <112182667+vangory@users.noreply.github.com>
2022-10-27 13:22:42 -0400

    Update cropMultiple.py

--- cropMultiple.py
+++ cropMultiple.py
@@ -1,7 +1,8 @@
-def cropMultiple(im, multiple):
+#takes in a file name and the multiple by which the height and width should be divisible, and returns the image cropped to fit those parameters
+def cropMultiple(filename, multiple):
+    im = Image.open(filename)
     newWidth = im.width - (im.width % multiple)
     newHeight = im.height - (im.height % multiple)
-    
     resizeImage = Image.new("RGB", (newWidth,newHeight))
     
     for i in range(newWidth):
