b34342475bb9990be49dd9215af535cf57c1d15a
lkoll123 <110080569+lkoll123@users.noreply.github.com>
2022-10-27 13:30:57 -0400

    Update Squarecrop.py

--- Squarecrop.py
+++ Squarecrop.py
@@ -1,7 +1,6 @@
 #takes in a file name and a side length(size)
 #returns the image cropped into a square and then scaled to the requested size for each side length
 def cropSquare(filename,size):
-    resizeImage = Image.new("RGB", (size,size))
     fullpath = 'C:\\Users\\lukad\\Algo 2022\\EmojiList\\' + filename
     im = Image.open(fullpath)
     if (im.width < im.height):
@@ -9,9 +8,10 @@ def cropSquare(filename,size):
     else:
         scale = size/im.height
     
-    for i in range(size):
-        for j in range(size):
-            r,g,b = im.getpixel((i/scale, j/scale))
-            resizeImage.putpixel((i,j), (r,g,b))
+    resizeImage = Image.new("RGB", (int(scale*im.width), int(scale*im.height)))
+    for i in range(0, resizeImage.width):
+        for j in range(0, resizeImage.height):
+            RGB = im.getpixel((int(i/scale), int(j/scale)))
+            resizeImage.putpixel((i, j), (RGB[0], RGB[1], RGB[2]))
 
     return resizeImage
