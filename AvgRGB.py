dc3d7a5244a649878b20363f7d0fbd7212622be3
vangory <112182667+vangory@users.noreply.github.com>
2022-11-01 09:42:55 -0400

    Update AvgRGB.py

--- AvgRGB.py
+++ AvgRGB.py
@@ -1,10 +1,10 @@
 #takes in a file name, top left corner coordinates of a part of an image, the width of the section, and the height.
 #returns the average RGB value in that area.
-def avgRGB(filename, startX, startY, width, height):    
+def avgRGB(filename, startX, startY, width, height, path):    
     r = 0
     g = 0
     b = 0
-    fullpath = 'C:\\Users\\lukad\\Algo 2022\\EmojiList\\' + filename
+    fullpath = path + '\\' + filename
     im = Image.open(fullpath)
     
     for i in range(startX, startX + width):
