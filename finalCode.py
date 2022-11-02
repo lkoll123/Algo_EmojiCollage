c3747ac28ce3f031655247178a3523f18de0ff84
vangory <112182667+vangory@users.noreply.github.com>
2022-11-01 20:57:55 -0400

    Update finalCode.py
    It tiles emojis now. . . but just seemingly random ones. . .

--- finalCode.py
+++ finalCode.py
@@ -114,10 +114,10 @@ def avgRGB(filename, startX, startY, width, height, path):
 #returns the index of the closest rgb value to the set value
 def findClosest(rgb, listOfRGBs):
     
+    minIndex = 0
     minDistance = (listOfRGBs[0][0] - rgb[0])**2 + (listOfRGBs[0][1] - rgb[1])**2 + (listOfRGBs[0][2] - rgb[2])**2
     for i in range(len(listOfRGBs)):
         currentDistance = 0
-        minIndex = 0
         for j in range(3):
             currentDistance += (listOfRGBs[i][j] - rgb[j])**2
         if (currentDistance < minDistance):
